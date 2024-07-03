62bd4ab61962170e37ece1786a0b39cbf54322fe
Reece Pritchard <reece@motorway.co.uk>
2024-07-03 16:36:25 +0100

    chore: clean up

--- src/main.py
+++ src/main.py
@@ -4,30 +4,5 @@ def main():
     text_node = TextNode("Hello, World!", "bold", "http://localhost:8888")
     
     print(text_node)
-    
-    markdown = """
-# Heading 1
-
-This is a paragraph.
-
-* Bullet point 1
-* Bullet point 2
-
-> This is a quote block.
-> This is a quote block as well.
-
-## Heading 2
-
-This is another paragraph.
-
-1. Ordered item 1
-2. Ordered item 2
-
-```
-print('Hello, World!')
-```
-"""
-    html_nodes = markdown_to_html_node(markdown)
-    print(html_nodes)
  
 main()
